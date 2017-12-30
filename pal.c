11735d4887925ad9cca91ec64195bfe5ea31e2b9
NachOP17 <ignaciotfw@gmail.com>
2017-12-30 17:56:52 -0400

    Cambie el nombre de una variable

--- pal.c
+++ pal.c
@@ -5,29 +5,28 @@
 #include <string.h>
 
 char *recorrerDirectorios(char directorio[]) {
-	struct dirent *sd;
-	struct dirent *sd2;
+	struct dirent *dentroDelDirectorio;
 	DIR *dir;
 	dir = opendir(directorio);
 	char hijos[20] = "";
-	while (sd = readdir(dir)) {
-		strcat(hijos, sd->d_name);
+	while (dentroDelDirectorio = readdir(dir)) {
+		strcat(hijos, dentroDelDirectorio->d_name);
 	}
 	unsigned int numero = strlen(hijos);
 	printf("Hijos: %s\n", hijos);
 	if (numero > 3) {
-		while (sd2 = readdir(dir)) {
-			printf("%s", sd2->d_name);
-			if ((sd2->d_name != ".") && (sd2->d_name != "..")) {
+		while (dentroDelDirectorio = readdir(dir)) {
+			printf("%s", dentroDelDirectorio->d_name);
+			if ((dentroDelDirectorio->d_name != ".") && (dentroDelDirectorio->d_name != "..")) {
 				strcat(directorio, "/");
-				strcat(directorio, sd2->d_name);
+				strcat(directorio, dentroDelDirectorio->d_name);
 				directorio = recorrerDirectorios(directorio);
 			}
 		}
 	} else {
-		closedir(dir);
 		return directorio;
 	}
+	closedir(dir);
 }
 
 int main(int argc, char *argv[]) {
