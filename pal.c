cdeb98dfd16eb9b8ab7248e4912bf7f96bb6ee74
fercon997 <fercon997@gmail.com>
2018-01-02 17:32:44 -0400

    Cambios en pal.c, recorrido de los directorios

--- pal.c
+++ pal.c
@@ -5,28 +5,56 @@
 #include <string.h>
 
 char *recorrerDirectorios(char directorio[]) {
-	struct dirent *dentroDelDirectorio;
-	DIR *dir;
-	dir = opendir(directorio);
+	struct dirent *dentroDelDirectorio = NULL;
+	DIR *pdir = NULL;
+	pdir = opendir(directorio);
 	char hijos[20] = "";
-	while (dentroDelDirectorio = readdir(dir)) {
-		strcat(hijos, dentroDelDirectorio->d_name);
-	}
-	unsigned int numero = strlen(hijos);
-	printf("Hijos: %s\n", hijos);
-	if (numero > 3) {
-		while (dentroDelDirectorio = readdir(dir)) {
-			printf("%s", dentroDelDirectorio->d_name);
-			if ((dentroDelDirectorio->d_name != ".") && (dentroDelDirectorio->d_name != "..")) {
-				strcat(directorio, "/");
-				strcat(directorio, dentroDelDirectorio->d_name);
-				directorio = recorrerDirectorios(directorio);
-			}
+	if (pdir == NULL) // if pdir wasn't initialised correctly
+	{ // print an error message and exit the program
+			printf("Aqui?");
+			printf ("\nERROR! pdir could not be initialised correctly");
+			exit (3);
+	} // end if
+	while (dentroDelDirectorio = readdir(pdir)) {
+		if (dentroDelDirectorio == NULL) // if pent has not been initialised correctly
+		{ // print an error message, and exit the program
+				printf("O aca?\n");
+				printf ("ERROR! pent could not be initialised correctly");
+				exit (3);
 		}
-	} else {
-		return directorio;
+		char DNUEVO[50] = "";
+		if ((strcmp(dentroDelDirectorio->d_name,".")) && (strcmp(dentroDelDirectorio->d_name,".."))){
+			strcat(DNUEVO, directorio);
+			strcat(hijos, dentroDelDirectorio->d_name);
+			strcat(DNUEVO, "/");
+			strcat(DNUEVO, dentroDelDirectorio->d_name);
+			printf("%s\n", DNUEVO);
+			recorrerDirectorios(DNUEVO);
+		}
 	}
-	closedir(dir);
+	// unsigned int numero = strlen(hijos);
+	// printf("Hijos: %s\n", hijos);
+	// printf("Numero: %i\n", numero );
+
+	// if (numero > 3) {
+	// 	while (dentroDelDirectorio = readdir(pdir)) {
+	// 		if (dentroDelDirectorio == NULL) // if pent has not been initialised correctly
+	// 		{ // print an error message, and exit the program
+	// 				printf("O aca?\n");
+	// 				printf ("ERROR! pent could not be initialised correctly");
+	// 				exit (3);
+	// 		}
+	// 		printf("%s", dentroDelDirectorio->d_name);
+	// 		if ((dentroDelDirectorio->d_name != ".") && (dentroDelDirectorio->d_name != "..")) {
+	// 			strcat(directorio, "/");
+	// 			strcat(directorio, dentroDelDirectorio->d_name);
+	// 			directorio = recorrerDirectorios(directorio);
+	// 		}
+	// 	}
+	// } //else {
+	// 	return directorio;
+	// //}
+	closedir(pdir);
 }
 
 int main(int argc, char *argv[]) {
@@ -34,4 +62,3 @@ int main(int argc, char *argv[]) {
 	printf("%s", recorrerDirectorios("./directoryTree"));
 	printf("\n");
 }
-
